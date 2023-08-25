b1e988fc9c30339e08e50ac9f808f38428b40c4e
Maanav Patel <mdhpatel@ad.unc.edu>
2023-08-25 16:19:31 -0400

    Finished EX00

---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,4 @@
+"""My first code for Comp110"""
+__author__ = "730710742"
+
+print("Hello, world, my name is maanav.")
---
+++ lessons/input.py
@@ -0,0 +1,3 @@
+"""Demonstrate asking the user or input"""
+
+print("Hello! " + input("What is your name? "))
